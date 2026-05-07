abc9bce71fbc80ac62821ad5401641f4df25f94f
Saumyaranjan Panigrahi <saumya.panigrahi19@gmail.com>
2026-05-07 07:13:46 +0530

    changed the folder name

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging # checking if the code executed correctly or not - the test code is at the end
+# import logging # checking if the code executed correctly or not - the test code is at the end
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -21,13 +22,13 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-# testing the code with one example...
-if __name__=="__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
+# # testing the code with one example...
+# if __name__=="__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)
 
 
 
